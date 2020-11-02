f3956fe611c53a95a18c545e5586b5319595a338
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2020-11-02 18:43:13 -0500

    Added Stylesheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     // The selector tells us where to plug the template
     selector: 'app-root',
-    template: '<p>Hello World</p>'
+    // This allows us to use an external html file for the template
+    templateUrl: './app.component.html',
+    // This allows us to use an external css file for the template
+    styleUrls: [
+        './app.component.css',
+    ],
 })
 export class AppComponent {}
