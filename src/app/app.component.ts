40816fea6e1b845414474b2f7a56f214b54dea72
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2020-11-02 18:33:57 -0500

    Bootstrapped a component and rendered a component

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // The selector tells us where to plug the template
+    selector: 'app-root',
+    template: '<p>Hello World</p>'
+})
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // Helps angular know its running in the browser
+import {AppComponent} from './app.component';
 
 // Tells angular what we should initially inject in the project
 @NgModule({
+    // The bootstrap stuff allows us to create an array of components we'd like to intialize when the app starts up
+    bootstrap: [
+        AppComponent,
+    ],
+    // We declare the AppComponent because its part of my application
+    declarations:[
+        AppComponent
+    ],
     // This takes an array of the modules we would like to import into our project
     imports: [
         BrowserModule,
